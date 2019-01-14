915c38484dc555577634e7b0c55babf48a3db891
Page Bowers <pabowers@microsoft.com>
2019-01-14 09:47:23 -0800

    making some fixes to the PR

--- deploy/vm/modules/ha_pair/outputs.tf
+++ deploy/vm/modules/ha_pair/outputs.tf
@@ -9,15 +9,6 @@ output "hdb1_ip" {
 output "hdb_vm_user" {
   value = "${var.vm_user}"
 }
-<<<<<<< HEAD
-
-output "windows_bastion_ip" {
-  value = "${module.windows_bastion_host.ip}"
-}
-
-output "windows_bastion_user" {
-  value = "${var.bastion_username_windows}"
-}
 
 output "primary_hdb" {
   value = "${module.create_hdb0.machine_hostname}"
@@ -26,5 +17,3 @@ output "primary_hdb" {
 output "secondary_hdb" {
   value = "${module.create_hdb1.machine_hostname}"
 }
-=======
->>>>>>> Moved Windows Bastion host creation to Ansible
